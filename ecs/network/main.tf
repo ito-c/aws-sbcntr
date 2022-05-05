a776a8ea4dfe4fa39060fa63148e3e3f15f97217
tashi1118 <tech.tashi1118.1991@gmail.com>
2022-05-05 23:40:20 +0900

    ecs service用のサブネットをdata only moduleにて用意

---
+++ ecs/network/main.tf
@@ -0,0 +1,21 @@
+data "aws_subnet" "private_container_1a" {
+  tags = {
+    Name = "sbcntr-NA-private-subnet-container-1a"
+  }
+}
+
+data "aws_subnet" "private_container_1c" {
+  tags = {
+    Name = "sbcntr-NA-private-subnet-container-1c"
+  }
+}
+
+output "private_container_1a_id" {
+  description = "id of the private subnet 1a"
+  value       = data.aws_subnet.private_container_1a.id
+}
+
+output "private_container_1c_id" {
+  description = "id of the private subnet 1c"
+  value       = data.aws_subnet.private_container_1c.id
+}
