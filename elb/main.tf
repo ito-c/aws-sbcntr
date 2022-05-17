9d45368fdf35d797675d811fea64e18248d1af98
tashi1118 <tech.tashi1118.1991@gmail.com>
2022-05-17 09:54:24 +0900

    [elb] target_groupのtarget_typeをFARGATE対応のIPにする

--- elb/main.tf
+++ elb/main.tf
@@ -109,10 +109,11 @@ resource "aws_lb_listener" "green" {
 
 # blue側ターゲットグループ
 resource "aws_lb_target_group" "blue" {
-  name     = "${local.project}-${local.environment}-tg-blue"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.network.vpc_id
+  name        = "${local.project}-${local.environment}-tg-blue"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.network.vpc_id
 
   health_check {
     protocol            = "HTTP"
@@ -134,10 +135,11 @@ resource "aws_lb_target_group" "blue" {
 
 # green側ターゲットグループ
 resource "aws_lb_target_group" "green" {
-  name     = "${local.project}-${local.environment}-tg-green"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = module.network.vpc_id
+  name        = "${local.project}-${local.environment}-tg-green"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.network.vpc_id
 
   health_check {
     protocol            = "HTTP"
